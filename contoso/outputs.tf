258e9f407b2f344686b435f50be72101f0701d5c
Your Name <james.jenkinsiv@sfgov.org>
2024-04-04 14:37:49 -0700

    updated ignore, completed lab 3.1

--- contoso/outputs.tf
+++ contoso/outputs.tf
@@ -1,10 +1,7 @@
 # outputs.tf
-output "jjcontoso_rg_id" {
-    value = azurerm_resource_group.jjcontoso_rg.id
-    description = "don't show actual data on cli output"
-    sensitive = true
-}
-
-output "jjcontoso_dev_rg_id" {    
-    value = azurerm_resource_group.jjcontoso_dev_rg.id
+output "rg_ids" {    
+    # splatting expression
+    value = azurerm_resource_group.rgs.*.id   
+    # or for expression
+    # value = [for rg in azurerm_resource_group.rgs : rg.id]     
 }
